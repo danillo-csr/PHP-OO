8cfe50534370bf4502eb35f7ab14b4287fd3fbf9
danillo cesar <danillocesar_06@hotmail.com>
2020-06-09 09:30:43 -0300

    mais alguns contatos com herança de atributos e métodos

--- 03-herança.php
+++ 03-herança.php
@@ -5,7 +5,7 @@
 
 PUBLIC - acessível por qualquer um, de qualquer lugar, não tem ocultação nenhuma
 
-PROTECTED - acessível por dentro da classe e por todos os herdeiros como se fossem
+PROTECTED - acessível por dentro da classe e por todos os herdeiros
 
 PRIVATE - acessível apenas dentro do escopo da classe
 
@@ -25,72 +25,157 @@ class Veiculo{
     private $numPassageiros;
     private $velMax;
 
+    //métodos públicos, são acessíveis de qualquer lugar, de dentro da class, de fora e de herdeiros da classe
     public function Andar(){
         echo "andou!<br>";
     }
 
-    public function Parar(){
+    protected function Parar(){
         echo "parou!<br>";
     }
 
-    public function getModelo(){
-        return $modelo;
+    private function Estacionar(){
+        echo "Estacionou!<br>";
     }
 
-    public function setModelo($m){
-        $this->modelo = $m;
-    }
-
-    public function getCor(){
-        return $cor;
-    }
-
-    public function setCor($c){
-        $this->cor = $c;
+    public function acessaEstacionar(){
+        return $this->Estacionar();
     }
     
-    public function getAno(){
-        return $ano;
-    }
+    //-------------------------------
 
-    public function setAno($a){
-        $this->ano = $a;
+    //métodos para alterar e recuperar as informações dos atributos $placa, $numPassageiros e $velMax, que tem o modificador de acesso private;
+    public function getPlaca(){
+        return $this->placa;
     }
-
+    public function setPlaca($p){
+        $this->placa = $p;
+    }
+    //-------------------------------
+    public function getNumPassageiros(){
+        return $this->numPassageiros;
+    }
+    public function setNumPassageiros($np){
+        $this->numPassageiros = $np;
+    }
+    //-------------------------------
+    public function getVelMax(){
+        return $this->velMax;
+    }
+    public function setVelMax($vm){
+        $this->velMax = $vm;
+    }
 }
 
 class Carro extends Veiculo {
     
-    $carro->categoria = "hatch";
-    $carro->motor = "1.8";
-    $carro->preco = 55000;
+    public function getCategoria(){
+        return $this->categoria;
+    }
+    public function setCategoria($cc){
+        $this->categoria = $cc;
+    }
+    //-------------------------------
+    public function getMotor(){
+        return $this->motor;
+    }
+    public function setMotor($m){
+        $this->motor = $m;
+    }
+    //-------------------------------
+    public function getPreco(){
+        return $this->preco;
+    }
+    public function setPreco($p){
+        $this->preco = $p;
+    }
 
+    public function acessaParar(){
+        return $this->Parar();
+    }
 
 }
 
 class Moto extends Veiculo {
 
-    $moto->categoria = "Custom";
-    $moto->motor = "800cc";
-    $moto->preco = 200000;
+    public function getCategoria(){
+        return $this->categoria;
+    }
+    public function setCategoria($cm){
+        $this->categoria = $cm;
+    }
+    //-------------------------------
+    public function getMotor(){
+        return $this->motor;
+    }
+    public function setMotor($m){
+        $this->motor = $m;
+    }
+    //-------------------------------
+    public function getPreco(){
+        return $this->preco;
+    }
+    public function setPreco($p){
+        $this->preco = $p;
+    }
+
+    public function acessaParar(){
+        return $this->Parar();
+    }
 
 }
 
+
+//cria o objeto $carro que é uma instancia da classe Carro;
+
 $carro = new Carro();
+
+//por terem o modificador de acesso Public, atribui valores diretamente aos atributos do objeto $carro, sem métodos específicos para isso(setters);
 
 $carro->modelo = "Gol";
 $carro->cor = "vermelho";
 $carro->ano = 1999;
 
-$carro->placa = "vkh-8946";
-$carro->numPassageiros = 5;
-$carro->velMax = 180;
+//por terem o modificador de acesso Public, recupera e mostra os valores diretamente dos atributos do objeto $carro, sem métodos específicos para isso(getters);
+
+echo $carro->modelo."<br>";
+echo $carro->cor."<br>";
+echo $carro->ano."<br>";
+
+//--------------------- altera e recupera o valor dos atributos $categoria, $motor e $preco, que tem o modificador de acesso protected
+
+$carro->setCategoria("Sedan");
+echo $carro->getCategoria()."<br>";
+
+$carro->setMotor("1.8");
+echo $carro->getMotor()."<br>";
+
+$carro->setPreco(60000);
+echo $carro->getPreco()."<br>";
+
+//--------------------- atribui e recupera os dados dos atributos $placa, $numPassageiros e $velMax, que estão com o modificador de acesso private, e só são acessíveis por seus
+//--------------------- respectivos métodos getters e setters
+
+$carro->setPlaca("xlr-8000");
+echo $carro->getPlaca()."<br>";
+
+$carro->setNumPassageiros(5);
+echo $carro->getNumPassageiros()."<br>";
+
+$carro->setVelMax(180);
+echo $carro->getVelMax()."<br>";
+
+//---------------------
 
 $carro->Andar();
-$carro->Parar();
-var_dump();
+$carro->acessaParar();
+$carro->acessaEstacionar();
 
-/* ------------------------------ */
+
+echo "<br>";
+var_dump($carro);
+
+/* ------------------------------------------------- */
 
 echo "<br><hr>";
 
@@ -100,10 +185,39 @@ $moto->modelo = "V-Max";
 $moto->cor = "Preto";
 $moto->ano = 2016;
 
-$moto->placa = "crf-2531";
-$moto->numPassageiros = 2;
-$moto->velMax = 170;
+//-------------------------
+
+echo $moto->modelo."<br>";
+echo $moto->cor."<br>";
+echo $moto->ano."<br>";
+
+//------------------------- 
+
+$moto->setCategoria("Custom");
+echo $moto->getCategoria()."<br>";
+
+$moto->setMotor("800cc");
+echo $moto->getMotor()."<br>";
+
+$moto->setPreco(200000);
+echo $moto->getPreco()."<br>";
+
+//-----------------------------
+
+$moto->setPlaca("gty-5736");
+echo $moto->getPlaca()."<br>";
+
+$moto->setNumPassageiros(2);
+echo $moto->getNumPassageiros()."<br>";
+
+$moto->setVelMax(180);
+echo $moto->getVelMax()."<br>";
+
+//---------------------
 
 $moto->Andar();
-$moto->Parar();
+$moto->acessaParar();
+$moto->acessaEstacionar();
+
+echo "<br>";
 var_dump($moto);
